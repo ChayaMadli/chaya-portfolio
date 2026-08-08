9d58731ca07d9b2915858ea4dd6b3e3548a48456
Chaya Madli <chayamadli@gmail.com>
2026-08-08 14:34:03 +0530

    Initial portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Chaya Madli</h1>
+      <p>Portfolio coming soon.</p>
+    </main>
+  )
+}
+
+export default App
+
